c28ef8893d086df75d7b724f9b5dbe42d7855fab
q10viking <1403090523@cau.edu.cn>
2019-04-05 01:05:21 +0800

    update at 01:05:17

--- DataLevel.py
+++ DataLevel.py
@@ -6,12 +6,13 @@ import logging
 logging.basicConfig(level=logging.DEBUG,format='%(asctime)s %(levelname)s %(message)s',datefmt='%Y-%m-%d %H:%M:%S')
 
 def store_date(content):
-    utc_dt = datetime.utcnow()
+    utc_dt = datetime.utcnow().replace(tzinfo=timezone.utc)
     bj_dt = utc_dt.astimezone(timezone(timedelta(hours=8)))
 
     bj_date = bj_dt.strftime("%Y-%m-%d")
     bj_time = bj_dt.strftime("%H:%M:%S")
 
+    print(bj_date,bj_time)
     file_end = "-log.csv"
     # 以月份来存储数据
     fname_csv = bj_date[:-3]+file_end
@@ -25,6 +26,7 @@ def store_date(content):
     with open(fname_csv,"a+") as f:
         f.write(row)
     logging.info("存储成功")
+    upload_github(bj_time)
 
 
 def create_file_csv(filename):
@@ -35,5 +37,13 @@ def create_file_csv(filename):
     logging.info(filename+"创建成功")
     
 
+def upload_github(bj_time):
+    os.system("git pull")
+    os.system("git add .")
+    os.system('git commit -m "{}"'.format("update at "+bj_time))
+    os.system("git push")
+    logging.info("已经提交到github")
+
 if __name__ == "__main__":
     store_date("test")
+
